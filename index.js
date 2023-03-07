3f92ad9b8d597a44a3233c3397b9458cfb925722
mpradobran <115194337+mpradobran@users.noreply.github.com>
2023-03-07 08:07:06 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+$(".name").animate({ top: "20px" }, 1000);
+
+$("h1").animate({ bottom: "450px" }, 1000);
+$(".description").animate({ bottom: "450px" }, 1000);
+
+$('.navbar-brand').hover(
+    function() {
+        var img = $('.img-princ');
+        img.attr('src', 'img/compu.gif');
+        setTimeout(function() {
+          img.attr('src', 'img/compu2.png');
+        }, 3000);
+      },
+      function() {
+        $(this).attr('src', 'img/compu2.png');
+      }  
+)
